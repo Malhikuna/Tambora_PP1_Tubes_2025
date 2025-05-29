4dea3db44ac500553191ff664a4a3c2806e5efe7
Limzy71 <muhikhsanjr05@gmail.com>
2025-05-29 18:38:48 +0700

    Feat: menambahkan atribut dan method baru ke dalam class Transaksi dan TransaksiService

--- src/main/java/logistik/Service/TransaksiService.java
+++ src/main/java/logistik/Service/TransaksiService.java
@@ -1,17 +1,46 @@
 package logistik.Service;
 
+import logistik.Config.DatabaseConnection;
+import logistik.Model.Transaksi;
+import logistik.Structure.StructureQueue;
+
+import java.sql.Connection;
+
 public class TransaksiService {
     /* Atribut */
-    
-    /* Prosedur & Fungsi */
+    private StructureQueue transaksiQueue;
+    private Connection conn;
+
+    // Constructor
+    public TransaksiService() {
+        this.transaksiQueue = new StructureQueue();
+        conn = DatabaseConnection.getConnection();
+    }
 
     // Prosedur untuk menambahkan transaksi baru ke queue
-
-    // Prosedur untuk mengambil transaksi terdepan dari queue
+    public void tambahTranksaksi(Transaksi transaksi) {
+        transaksiQueue.enqueue(transaksi);
+        System.out.println("Transaksi berhasil ditambahkan ke antrian.");
+    }
 
     // Prosedur untuk menampilkan semua transaksi dalam queue
+    public void tampilkanAntrian() {
+        System.out.println("Daftar Transaksi dalam Antrian:");
+        transaksiQueue.displayElement();
+    }
 
     // Prosedur untuk menampilkan jumlah antrian queue
+    public void jumlahAntrian() {
+        System.out.println("Jumlah transaksi dalam antrian: " + transaksiQueue.size());
+    }
 
     // Prosedur untuk melihat transaksi terdepan tanpa menghapus
+    public void lihatAntrianTerdepan() {
+        if (!transaksiQueue.isEmpty()) {
+            Transaksi transaksi = (Transaksi) transaksiQueue.front();
+            System.out.println("Transaksi terdepan: " + transaksi);
+        } else {
+            System.out.println("Antrian kosong.");
+        }
+    }
 }
