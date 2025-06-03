9b724641a52afc3b2842cf2e576e008a5a8f2ba6
ahmaddzakis <ahmaddzaki141@gmail.com>
2025-06-03 23:03:27 +0700

    fix(login): perbaiki tampilan dan logika pada login controller dan scene switcher

--- src/main/java/logistik/App.java
+++ src/main/java/logistik/App.java
@@ -1,10 +1,23 @@
 package logistik;
 
 import javafx.application.Application;
-import logistik.view.Menu;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/logistik/view/LoginView.fxml"));
+        Scene scene = new Scene(loader.load());
+        scene.getStylesheets().add(getClass().getResource("/logistik/style/login.css").toExternalForm());
+
+        primaryStage.setTitle("Sistem Logistik");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        Application.launch(Menu.class, args);
+        launch(args);
     }
 }
