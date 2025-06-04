2eded086712ea0a86822dafdc9713709b51760b6
ahmaddzakis <ahmaddzaki141@gmail.com>
2025-06-04 23:29:49 +0700

    fix(barang): perbaikan tampilan dan logika awal fitur barang

--- src/main/java/logistik/controller/barang/BarangFormController.java
+++ src/main/java/logistik/controller/barang/BarangFormController.java
@@ -1,4 +1,11 @@
 package logistik.controller.barang;
 
+import javafx.event.ActionEvent;
+
 public class BarangFormController {
+    public void handleSimpanButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleBatalButtonAction(ActionEvent actionEvent) {
+    }
 }
