80dcddfe16a3c5bd7afc711fde32bcfc15514187
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-06-07 03:26:17 +0700

    feat(controller, view): tambah fitur tambah lihat, cari, dan hapus Barang di JavaFX

--- src/main/java/logistik/controller/barang/BarangFormController.java
+++ src/main/java/logistik/controller/barang/BarangFormController.java
@@ -1,6 +1,22 @@
 package logistik.controller.barang;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+// import javafx.stage.Stage; // Tidak lagi mengontrol Stage sendiri jika di-embed
+import logistik.App;
+import logistik.model.Barang;
+import logistik.model.Kategori;
+import logistik.service.BarangService;
+import logistik.service.KategoriService;
+
+import java.sql.SQLException;
 
 public class BarangFormController {
     public void handleSimpanButtonAction(ActionEvent actionEvent) {
